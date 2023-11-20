c49bcef1c864789fea4752c9ab219959ac55775a
Milton Luca Schirmer <97521508+SirParadise@users.noreply.github.com>
2023-11-20 20:40:24 -0300

    Delete API directory

--- API/API/API/Models/Categoria.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace API.Models;
-
-public class Categoria
-{
-    public int CategoriaId { get; set; }
-    public string? Nome { get; set; }
-    public DateTime CriadoEm { get; set; } = DateTime.Now;
-}
